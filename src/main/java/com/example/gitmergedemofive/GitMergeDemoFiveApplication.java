de836b4c06541dd0be65135bd5b15c1b1a9a659b
王钰 <wangyu9@bonc.com.cn>
2022-01-17 22:57:50 +0800

    master:提交远程master分支

---
+++ src/main/java/com/example/gitmergedemofive/GitMergeDemoFiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitmergedemofive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMergeDemoFiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitMergeDemoFiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitmergedemofive/GitMergeDemoFiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitmergedemofive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitMergeDemoFiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
